043f285ed6dd990120d79d1bf89368f185e464ba
mournvine <adialamsyah.dev@gmail.com>
2020-05-09 19:02:58 +0700

    add fallback

--- sw.js
+++ sw.js
@@ -1,9 +1,61 @@
+const staticCacheName = 'site-static-v2';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html'
+];
+
 // install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker has been isntall')
+    // console.log('service worker has been isntall')
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('chaching all assets')
+            cache.addAll(assets)
+        })
+    )
 })
 
 //active service worker
 self.addEventListener('active', evt => {
-    console.log('service worker has been actived')
+    // console.log('service worker has been actived')
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            // console.log(keys)
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete())
+            )
+        })
+    )
+})
+
+self.addEventListener('fetch', evt => {
+    // console.log('fetch event ', evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request)
+                .then(fetchRes => {
+                    return caches.open(dynamicCacheName)
+                        .then(cache => {
+                            cache.put(evt.request.url, fetchRes.clone());
+                            return fetchRes
+                        })
+                })
+        }).catch(() => {
+            if (evt.request.url.indexOf('.html') > -1) {
+                return caches.match('/pages/fallback.html')
+            }
+
+        })
+    )
 })
